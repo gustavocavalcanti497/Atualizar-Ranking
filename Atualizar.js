54216efb9c1dc75695d72d97939caeaaafdee48b
gustavocavalcanti497 <158003075+gustavocavalcanti497@users.noreply.github.com>
2024-12-23 14:59:30 -0300

    Atualizando script

---
+++ Atualizar.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+// APIs a serem atualizadas
+const endpoints = ['http://arneltem-rest.vistahost.com.br/ranking?key=f83ce208ed84db547bedf4824205d042', 'http://gralhaim-rest.vistahost.com.br/ranking?key=0869be516bbd5ee49d545af01cd6e22d'];
+
+
+//Mapeando os links
+async function carregarUrls(){
+    
+    const promises = endpoints.map((url) => requisicao(url));
+    const results = await Promise.all(promises);
+
+    results.forEach((result) => {
+        if (result.error) {
+            console.error(`Erro no endpoint ${result.url}: ${result.error}\n`);
+        } else {
+            console.log(`Resposta do endpoint ${result.url}:`, result.data, '\n');
+        }
+    });
+}
+
+// Rodando os gets
+async function requisicao(url) {
+    try{
+        const response = await axios.get(url);
+        return { url, data: response.data};
+    } catch (error){
+        return{
+            url, error: error.response ? `Erro ${error.response.status}: ${error.response.statusText}`: `Erro na requisição: ${error.message}`
+        }
+    }
+    
+}
+
+carregarUrls();
+
