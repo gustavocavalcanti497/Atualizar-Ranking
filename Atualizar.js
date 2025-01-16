b9781988d6bb4323808085415f11db7dd4563065
gustavocavalcanti497 <158003075+gustavocavalcanti497@users.noreply.github.com>
2025-01-16 11:01:49 -0300

    Update Atualizar.js

--- Atualizar.js
+++ Atualizar.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 // APIs a serem atualizadas
-const endpoints = ['http://realmapi9867-rest.vistahost.com.br/ranking?key=b95c5684a1c3c47e4dda03158ed2fbf3','http://alexsand-rest.vistahost.com.br/ranking?key=c31f1528695709356a116813b56e0db9','http://arneltem-rest.vistahost.com.br/ranking?key=f83ce208ed84db547bedf4824205d042', 'http://gralhaim-rest.vistahost.com.br/ranking?key=0869be516bbd5ee49d545af01cd6e22d', 'http://rafaella18114-rest.vistahost.com.br/ranking?key=d0ce01201f2d53eaa939a131057bfcaf'];
+const endpoints = ['http://realmapi9867-rest.vistahost.com.br/ranking?key=b95c5684a1c3c47e4dda03158ed2fbf3','http://loboimov-rest.vistahost.com.br/ranking?key=b1a9cc6d31340695e8bd824f76e4671a','http://alexsand-rest.vistahost.com.br/ranking?key=c31f1528695709356a116813b56e0db9','http://arneltem-rest.vistahost.com.br/ranking?key=f83ce208ed84db547bedf4824205d042', 'http://gralhaim-rest.vistahost.com.br/ranking?key=0869be516bbd5ee49d545af01cd6e22d', 'http://rafaella18114-rest.vistahost.com.br/ranking?key=d0ce01201f2d53eaa939a131057bfcaf'];
 
 const axiosInstance = axios.create({
     headers: {
